aadac8cbafc78e51f15f4e44a7e1f8aad0f31ade
Paul Sammut <paul.c.sammut@gmail.com>
2020-05-20 16:47:43 -0700

    added mac stuff

--- auto_compile.sh
+++ auto_compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+MAC_XELATEX="/usr/local/texlive/2020basic/bin/x86_64-darwin/xelatex"
+
 # kill any pdf open
 pkill acro
 
@@ -8,3 +10,16 @@ cd ~/resume/base
 
 # grab all the source files
 tree -if ../ | grep "\.tex\|\.cls" | entr xelatex resume
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Mac OS X platform
+    LATEX="/usr/local/texlive/2020basic/bin/x86_64-darwin/xelatex"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Linux
+    LATEX="xelatex"
+else
+    LATEX="ERROR"
+fi
+
+
+tree -if ../ | grep "\.tex\|\.cls" | entr $LATEX resume
